2f5369574e7166c50250729edef9736560f90cca
David Fanaro <davidf4321@hotmail.com>
2020-08-25 18:20:52 -0400

    updated config

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,18 +7,19 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     if let databaseURL = Environment.get("DATABASE_URL") {
-    app.databases.use(try .postgres(
-        url: databaseURL
-        ), as: .psql)
+        app.databases.use(try .postgres(
+            url: databaseURL
+            ), as: .psql)
     }
-
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "",
-        username: Environment.get("DATABASE_USERNAME") ?? "",
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? ""
-    ), as: .psql)
-
+    else{
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "",
+            username: Environment.get("DATABASE_USERNAME") ?? "",
+            password: Environment.get("DATABASE_PASSWORD") ?? "",
+            database: Environment.get("DATABASE_NAME") ?? ""
+            ), as: .psql)
+    }
+    
     app.migrations.add([UserMigration(),DropMigration(),TokenMigration()])
     
     // register routes
